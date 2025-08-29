7822ce2de00838dc40c35ca41061867ba3961d91
ANdinho10 <andinho.as10@hotmail.com>
2025-08-29 09:58:56 -0300

    Adicionando a Model

---
+++ Codigo/Restaurante/RestauranteWeb/Models/MesaModel.cs
@@ -0,0 +1,30 @@
+﻿using Core;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestauranteWeb.Models
+{
+    public class MesaModel
+    {
+        [Display(Name = "Código")]
+        [Key]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public int Id { get; set; }
+
+        [Display(Name = "Número")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public int Numero { get; set; }
+
+        [Display(Name = "Capacidade")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public int Capacidade { get; set; }
+
+        [Display(Name = "Restaurante")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public int IdRestaurante { get; set; }
+
+        public virtual Restaurante IdRestauranteNavigation { get; set; } = null!;
+
+        public virtual ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+
+    }
+}
