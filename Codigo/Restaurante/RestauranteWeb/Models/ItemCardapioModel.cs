7153809afcb9af609a7c8e27ba9b73dbb134bed5
Pedro William <pewilliam.ac@outlook.com>
2025-08-29 00:24:39 -0300

    Adicionado o ViewModel de ItemCardapio.

---
+++ Codigo/Restaurante/RestauranteWeb/Models/ItemCardapioModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestauranteWeb.Models
+{
+    public class ItemCardapioModel
+    {
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public uint Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+        public string Nome { get; set; } = null!;
+
+        [Display(Name = "Preço")]
+        [DataType(DataType.Currency)]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public decimal? Preco { get; set; }
+
+        [Display(Name = "Detalhes")]
+        [StringLength(500, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+        public string? Detalhes { get; set; }
+
+        [Display(Name = "Ativo")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public sbyte Ativo { get; set; }
+
+        [Display(Name = "Disponível")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(45, MinimumLength = 2, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+        public string Disponivel { get; set; } = null!;
+
+        [Display(Name = "Restaurante")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public uint IdRestaurante { get; set; }
+
+        [Display(Name = "Grupo de Cardápio")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public uint IdGrupoCardapio { get; set; }
+    }
+}
