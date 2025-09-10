eaed5b9f8e80a412a5af8c1bcf7b6b720065b184
VictorTechrb <victortechrb@gmail.com>
2025-09-10 11:48:44 -0300

    Home adicionado

--- Codigo/Restaurante/RestauranteWeb/Controllers/HomeController.cs
+++ Codigo/Restaurante/RestauranteWeb/Controllers/HomeController.cs
@@ -1,20 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using RestauranteWeb.Models;
+using Core.Service;
+using AutoMapper;
 
 namespace RestauranteWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRestauranteService _restauranteService;
+        private readonly IItemCardapioService _itemCardapioService;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRestauranteService restauranteService, IItemCardapioService itemCardapioService, IMapper mapper)
         {
             _logger = logger;
+            _restauranteService = restauranteService;
+            _itemCardapioService = itemCardapioService;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
+            var restaurantes = _restauranteService.GetAll().Take(6).ToList();
+            var cardapios = _itemCardapioService.GetAll().Take(6).ToList();
+            ViewBag.Restaurantes = restaurantes;
+            ViewBag.Cardapios = cardapios;
             return View();
         }
 
